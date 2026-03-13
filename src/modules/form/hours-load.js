81db47b2d25ca4f7402f391a9476f97381dc6a0c
Jhonatan <jhpss1@outlook.com>
2026-03-13 09:37:49 -0300

    Add => Adição de listagem de horarios e separando por periodos

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,17 +1,46 @@
-import { openingHours } from "../../utils/opening-hours.js";
 import dayjs from "dayjs";
+import { openingHours } from "../../utils/opening-hours.js";
+const hours = document.getElementById("hours")
 
 export function hoursLoad(date){
     const opening = openingHours.map((hour)=> {
         //recupera somente a hora
         const [scheduleHour] = hour.split(":")
 
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
         
         return{
             hour,
             avaliable: !isHourPast,
         }
     })
-    console.log(opening)
+
+    //renderizar os horários
+
+    opening.forEach(({hour, avaliable}) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(avaliable ? "hour-avaliable" : "hour-unavailable")
+
+        li.textContent = hour
+
+        if(hour==="9:00"){
+            hourHeaderAdd("Manhã")
+        } else if (hour === "13:00"){
+            hourHeaderAdd("Tarde")
+        } else if (hour=== "18:00"){
+            hourHeaderAdd("noite")
+        }
+
+        hours.append(li)
+    })
+}
+
+function hourHeaderAdd(title){
+    const header = document.createElement("li")
+    header.classList.add("hour-period")
+    header.textContent = title
+    
+    hours.append(header)
 }
